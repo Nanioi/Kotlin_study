cd4a1e11fcbef2b0efd8ab2f6e175be5d71b7662
Nanioi <0802ny@gmail.com>
2021-04-10 15:56:20 +0900

    mellon_activity
    - 노래재생
    - 앱 종료 시 실행종료
    - 노래 도중 다른 노래 재생 시 기존 재생공 중지 후 다른 노래 재생

---
+++ app/src/main/java/com/example/myapplication/Song.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+import java.io.Serializable
+
+class Song(
+    val title: String? = null,
+    val thumnail: String? = null,
+    val song: String? = null
+):Serializable
